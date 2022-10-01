22fa72a087af9e274b095c3270db83056747309f
domovoi <vadimshergin@gmail.com>
2022-10-01 20:41:21 +0700

    Main issue: to develop tables creation Result: creation, insertion methods have been created Additional tasks: --- Module status: under development

--- ORM.py
+++ ORM.py
@@ -7,6 +7,21 @@ class BaseManagement:
         con = sqlite3.connect(bd_name)
         self.cursor = con.cursor()
 
+    def insert_column(self, table_name, column_name, column_type):
+        self.cursor.execute(
+            f'ALTER TABLE {table_name}'
+            f'ADD COLUMN {column_name} {column_type.upper()};'
+        )
+        return print(f'Column {column_name} has been added')
+
     def create_table(self, *args):
-        ''' Use the following construction [RAW_NAME, RAW_TYPE]. Type by default is TEXT.
-            The very first argument must contain only a table [NAME]'''
+        """ Use the following construction [column_name, column_type]. Type by default is TEXT.
+            The very first argument must contain only a table [name]"""
+
+        self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]}'
+                            f'  (id INTEGER PRIMARY KEY AUTOINCREMENT);')
+
+        for arg in args[1:]:
+            self.insert_column(args[0], arg[0], arg[1])
+
+        return print(f'Table {args[0]} has been created')
