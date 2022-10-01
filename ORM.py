26ab4a2d89195a5f25fcfeee3d16d6544f779440
domovoi <vadimshergin@gmail.com>
2022-10-01 21:11:09 +0700

    Main issue: to add an errors' handler for create method Result: done Additional tasks: --- Module status: under development

--- ORM.py
+++ ORM.py
@@ -12,16 +12,25 @@ class BaseManagement:
             f'ALTER TABLE {table_name}'
             f'ADD COLUMN {column_name} {column_type.upper()};'
         )
-        return print(f'Column {column_name} has been added')
+        return print(f'Column {column_name} with type {column_type.upper()} has been added')
 
     def create_table(self, *args):
         """ Use the following construction [column_name, column_type]. Type by default is TEXT.
             The very first argument must contain only a table [name]"""
 
-        self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]}'
-                            f'  (id INTEGER PRIMARY KEY AUTOINCREMENT);')
+        current_arg = None
 
-        for arg in args[1:]:
-            self.insert_column(args[0], arg[0], arg[1])
+        try:
+            self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]}'
+                                f'  (id INTEGER PRIMARY KEY AUTOINCREMENT);')
+            for arg in args[1:]:
+                current_arg = arg[0]
+                if len(arg) > 1:
+                    self.insert_column(args[0], arg[0], arg[1])
+                else:
+                    self.insert_column(args[0], arg[0], 'text')
+        except sqlite3.OperationalError:
+            return print(f'A column {current_arg.upper()} you tried to create is already exists. Check you request for '
+                         f'other duplicates')
 
-        return print(f'Table {args[0]} has been created')
+        return print(f'Table {args[0][0]} has been created')
