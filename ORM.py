39fa4e4964f231e6cf8d134eb74cf061bffbd934
domovoi <vadimshergin@gmail.com>
2022-10-02 13:57:05 +0700

    Main issue: to solve the problem with creation of tables Result: done Additional tasks: --- Module status: under development. Working

--- ORM.py
+++ ORM.py
@@ -4,33 +4,41 @@ import sqlite3
 class BaseManagement:
 
     def __init__(self, bd_name):
-        con = sqlite3.connect(bd_name)
+        self.bd_name = bd_name
+        con = sqlite3.connect(self.bd_name)
         self.cursor = con.cursor()
 
     def insert_column(self, table_name, column_name, column_type):
-        self.cursor.execute(
-            f'ALTER TABLE {table_name}'
-            f'ADD COLUMN {column_name} {column_type.upper()};'
-        )
+        try:
+            self.cursor.execute(
+                f'ALTER table {table_name}'
+                f'ADD COLUMN IF NOT EXISTS {column_name} {column_type.upper()};'
+            )
+        except sqlite3.OperationalError:
+            return print(f'A column {column_name} you tried to create is already exists. Check you request for '
+                         f'other duplicates')
         return print(f'Column {column_name} with type {column_type.upper()} has been added')
 
     def create_table(self, *args):
         """ Use the following construction [column_name, column_type]. Type by default is TEXT.
             The very first argument must contain only a table [name]"""
 
-        current_arg = None
+        query_body = str()
+        for arg in args[1:]:
+            if len(arg) > 1:
+                query_body += f'{arg[0]} {arg[1].upper()},\n'
+            else:
+                query_body += f'{arg[0]} TEXT,\n'
 
-        try:
-            self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]}'
-                                f'  (id INTEGER PRIMARY KEY AUTOINCREMENT);')
-            for arg in args[1:]:
-                current_arg = arg[0]
-                if len(arg) > 1:
-                    self.insert_column(args[0], arg[0], arg[1])
-                else:
-                    self.insert_column(args[0], arg[0], 'text')
-        except sqlite3.OperationalError:
-            return print(f'A column {current_arg.upper()} you tried to create is already exists. Check you request for '
-                         f'other duplicates')
+        self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]} ('
+                            f'id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
+                            f'{query_body[:-2]}\n'
+                            f')')
 
         return print(f'Table {args[0]} has been created')
+
+    def select(self, selector, table_name):
+        self.cursor.execute(
+            f'SELECT {selector} FROM {table_name}'
+        )
+        print(self.cursor.fetchall())
