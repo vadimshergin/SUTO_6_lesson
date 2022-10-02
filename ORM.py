4c34ca4a3ebb27937b9b31b841669e46949cbcab
domovoi <vadimshergin@gmail.com>
2022-10-02 14:26:22 +0700

    Main issue: increase functionality Result: TODO tasks are added Additional tasks: --- Module status: under development. Working

--- ORM.py
+++ ORM.py
@@ -15,15 +15,15 @@ class BaseManagement:
                 f'ADD COLUMN IF NOT EXISTS {column_name} {column_type.upper()};'
             )
         except sqlite3.OperationalError:
-            return print(f'A column {column_name} you tried to create is already exists. Check you request for '
+            return print(f'A column -- {column_name} -- you tried to create is already exists. Check you request for '
                          f'other duplicates')
         finally:
             self.con.commit()
-        return print(f'Column {column_name} with type {column_type.upper()} has been added')
+        return print(f'Column -- {column_name} -- with type -- {column_type.upper()} -- has been ADDED')
 
     def create_table(self, *args):
         """ Use the following construction [column_name, column_type]. Type by default is TEXT.
-            The very first argument must contain only a table [name]"""
+            The very first argument must contain only a string with a table name"""
 
         query_body = str()
         for arg in args[1:]:
@@ -37,8 +37,9 @@ class BaseManagement:
                             f'{query_body[:-2]}\n'
                             f');')
         self.con.commit()
-        return print(f'Table {args[0]} has been created')
+        return print(f'Table -- {args[0]} -- has been CREATED')
 
+#TODO increase functionality of the method
     def select(self, selector, table_name):
         self.cursor.execute(
             f'SELECT {selector} FROM {table_name}'
@@ -48,3 +49,12 @@ class BaseManagement:
     def drop_table(self, table_name):
         self.cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
         self.con.commit()
+        return print(f'The table -- {table_name} -- has been REMOVED')
+
+#TODO add insert method
+
+#TODO add update method
+
+#TODO add delete method
+
+#TODO add foreign key injection
