8919795e3924a0dd453fd965ff6336e4148eac7f
domovoi <vadimshergin@gmail.com>
2022-10-02 18:55:49 +0700

    Main issue: to add foreign key functionality Result: done Additional tasks: multidrop added --- Module status: under development. Working

--- ORM.py
+++ ORM.py
@@ -8,7 +8,7 @@ class BaseManagement:
         self.con = sqlite3.connect(self.bd_name)
         self.cursor = self.con.cursor()
 
-    def insert_column(self, table_name, column_name, column_type):
+    def add_column(self, table_name, column_name, column_type):
         try:
             self.cursor.execute(
                 f'ALTER table {table_name}'
@@ -21,9 +21,11 @@ class BaseManagement:
             self.con.commit()
         return print(f'Column -- {column_name} -- with type -- {column_type.upper()} -- has been ADDED')
 
-    def create_table(self, *args):
+    def create_table(self, *args, foreign_key=None, references=None):
         """ Use the following construction [column_name, column_type]. Type by default is TEXT.
-            The very first argument must contain only a string with a table name"""
+            In [column_type] you can set PRIMARY KEY, AUTOINCREMENT, NOT NULL etc: ['office_name', 'text not null'].
+            To set foreign_key='table_key' and references='table_name table_key'.
+            The very first argument must contain only a string with a table name. """
 
         query_body = str()
         for arg in args[1:]:
@@ -32,9 +34,13 @@ class BaseManagement:
             else:
                 query_body += f'{arg[0]} TEXT,\n'
 
+        if foreign_key is not None:
+            foreign_key = f',\nFOREIGN KEY ({foreign_key})'
+            references = f'REFERENCES {references.split()[0]} ({references.split()[1]})'
+
         self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]} ('
                             f'id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
-                            f'{query_body[:-2]}\n'
+                            f'{query_body[:-2]}{foreign_key} {references}'
                             f');')
         self.con.commit()
         return print(f'Table -- {args[0]} -- has been CREATED')
@@ -46,8 +52,9 @@ class BaseManagement:
         )
         print(self.cursor.fetchall())
 
-    def drop_table(self, table_name):
-        self.cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
+    def drop_table(self, *table_name):
+        for name in table_name:
+            self.cursor.execute(f'DROP TABLE IF EXISTS {name};')
         self.con.commit()
         return print(f'The table -- {table_name} -- has been REMOVED')
 
@@ -56,5 +63,3 @@ class BaseManagement:
 #TODO add update method
 
 #TODO add delete method
-
-#TODO add foreign key injection
