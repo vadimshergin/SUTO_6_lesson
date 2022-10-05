eaca097037d4069f3ceb4a8b6df06f69ff820b9d
domovoi <vadimshergin@gmail.com>
2022-10-05 10:53:28 +0700

    Main issue: to add an update method Result: done Additional tasks: --- Module status: under development. Working

--- ORM.py
+++ ORM.py
@@ -8,11 +8,26 @@ class BaseManagement:
         self.con = sqlite3.connect(self.bd_name)
         self.cursor = self.con.cursor()
 
+    def type_adapter(self, what_to_adapt):
+        try:
+            types = {
+                'int': 'INTEGER',
+                'integer': 'INTEGER',
+                'float': 'REAL',
+                'str': 'STRING',
+                'string': 'STRING',
+                'bytes': 'BLOB',
+                'None': 'NULL'
+            }
+            return types[what_to_adapt]
+        except KeyError:
+            return print(f'The type of value -- {what_to_adapt} -- is not acceptable')
+
     def add_column(self, table_name, column_name, column_type):
         try:
             self.cursor.execute(
                 f'ALTER table {table_name}'
-                f'ADD COLUMN IF NOT EXISTS {column_name} {column_type.upper()};'
+                f'ADD COLUMN IF NOT EXISTS {column_name} {self.type_adapter(column_type)};'
             )
         except sqlite3.OperationalError:
             return print(f'ERROR OCCURRED! The column -- {column_name} -- you tried to create is already exists. '
@@ -29,17 +44,16 @@ class BaseManagement:
 
         query_body = str()
         for arg in args[1:]:
-            if len(arg) > 1:
-                query_body += f'{arg[0]} {arg[1].upper()},\n'
-            else:
+            if len(arg) < 1 or arg[1] not in ['int', 'integer', 'float', 'str', 'string', 'bytes']:
                 query_body += f'{arg[0]} TEXT,\n'
+            else:
+                query_body += f'{arg[0]} {self.type_adapter(arg[1])},\n'
 
         if foreign_key is not None:
             foreign_key = f',\nFOREIGN KEY ({foreign_key})'
             references = f'REFERENCES {references.split()[0]} ({references.split()[1]})'
 
         self.cursor.execute(f'CREATE TABLE IF NOT EXISTS {args[0]} ('
-                            #f'id INTEGER PRIMARY KEY AUTOINCREMENT,\n'
                             f'{query_body[:-2]}{foreign_key} {references}'
                             f');')
         self.con.commit()
@@ -59,7 +73,8 @@ class BaseManagement:
         return print(f'The table -- {table_name} -- has been REMOVED')
 
     def insert(self, table_name, **kwargs):
-        """ Use key word construction 'onepiece'='argument, argument, etc' for inserting row at once. """
+        """ Use key word construction 'onepiece'='argument, argument, etc' for inserting row at once.
+            Use named argument for inserting certain key-value pairs."""
         if kwargs.get('onepiece') is not None:
             try:
                 values = tuple([i.strip() for i in kwargs['onepiece'].split(',')])
@@ -85,6 +100,16 @@ class BaseManagement:
             except sqlite3.OperationalError:
                 return print('ERROR OCCURRED! Check columns names in your query')
 
-#TODO add update method
+    def update(self, table_name, target, **kwargs):
+        """ Use kwargs for setting the parameters to update.
+            Target for setting WHERE conditions. """
+        query_body = str()
+        for key, value in kwargs.items():
+            query_body += f"'{key}'='{value}', "
+        self.cursor.execute(
+            f'UPDATE {table_name} SET {query_body[:-2]} WHERE {target};'
+        )
+        self.con.commit()
+        return print(f'Update operation for table -- {table_name} -- has been done')
 
 #TODO add delete method
